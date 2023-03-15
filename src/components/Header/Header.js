4f0f119dacd8fb03c21b66b774213ee537d8d32f
MD Roknuzzaman <rokonmd111@gmail.com>
2023-03-15 15:50:26 +0600

    done with submit form and sign out button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { userContext } from '../../App';
 import logo from '../../images/logo.png'
 import './Header.css';
 
 const Header = () => {
+    const [loggedInUser, setLoggedInUser] = useContext(userContext)
     return (
         <div className='header'>
             <img src={logo} alt="" />
@@ -11,6 +13,7 @@ const Header = () => {
                 <Link to="/shop">Shop</Link>
                 <Link to="/review">Order Review</Link>
                 <Link to="/manage">Manage Inverntory</Link>
+                <button onClick={() => setLoggedInUser({})}>Sign Out</button>
             </nav>
         </div>
     );
--- src/components/Shipment/Shipment.js
+++ src/components/Shipment/Shipment.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { useForm } from 'react-hook-form';
+import { userContext } from '../../App';
+import './Shipment.css'
 
 const Shipment = () => {
-    
-    return (
-        <div>
-            <h1>this is Shipment</h1>
-        </div>
-    );
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const [loggedInUser, setLoggedInUser] = useContext(userContext)
+    const onSubmit = data => console.log(data);
+
+  return (
+    <form className='ship-form' onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue={loggedInUser.name} {...register("name", { required: true })} placeholder='Your Name'/>
+      {errors.name && <span className='error'>Name is required</span>}
+      
+      <input defaultValue={loggedInUser.email} {...register("email", { required: true })}  placeholder='Your Email'/>
+      {errors.email && <span className='error'>Email is required</span>}
+      
+      <input {...register("address", { required: true })}  placeholder='Your Address'/>
+      {errors.address && <span className='error'>Address is required</span>}
+      
+      <input {...register("phone", { required: true })}  placeholder='Your Phone Number'/>
+      {errors.phone && <span className='error'>Phone Number is required</span>}
+      
+      <input type="submit" />
+    </form>
+  );
 };
 
 export default Shipment;
